1fee8986cdf4bef6cdeb65fc37121561ccf1b18a
Valtteri <savonen.emppu@gmail.com>
2025-02-19 16:15:36 +0200

    correct client.js

--- packages/create-valzu-app/template/pages/index.ts
+++ packages/create-valzu-app/template/pages/index.ts
@@ -1,4 +1,4 @@
-import WelcomePage from "../../components/WelcomePage";
+import WelcomePage from "../components/WelcomePage"
 
 export default function Home() {
   return WelcomePage();
--- packages/valzu-core/dist/server.js
+++ packages/valzu-core/dist/server.js
@@ -54,7 +54,7 @@ export function useServer(options) {
     });
     // Serve static files from the public directory.
     app.use(express.static(publicDir));
-    app.use("/client.js", express.static(path.resolve(cwd, "dist/client.js")));
+    app.use("/client.js", express.static(path.resolve(cwd, ".build/client.js")));
     // Determine the port and start listening.
     const port = (options === null || options === void 0 ? void 0 : options.port) || Number(process.env.PORT) || 3000;
     app.listen(port, () => {
